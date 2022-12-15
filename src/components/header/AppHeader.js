aa984d599eb543c71b5bdb5f365d951905ba1931
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2022-12-15 23:14:53 +0100

    menu magazyn

--- src/components/header/AppHeader.js
+++ src/components/header/AppHeader.js
@@ -1,14 +1,14 @@
 import React from "react";
 import classes from "./AppHeader.module.css";
 import {Link} from "react-router-dom";
-import { Container} from "react-bootstrap";
+import { Container, Button, ButtonGroup, Dropdown} from "react-bootstrap";
 const AppHeader = () => {
     return (
         <div  className={classes.AppHeader}>
 
             <div  className={classes.HeaderLeft}>
-                <img src={"https://picsum.photos/id/146/100/50"}/>
-                    <div>Mały Wypożycza Narty</div>
+                <img src={"https://picsum.photos/id/146/110/60"}/>
+                    <h5>Mały Wypożycza Narty</h5>
             </div>
 
             <Container className={classes.HeaderRight} >
@@ -18,18 +18,22 @@ const AppHeader = () => {
                 <Link to={"/formKlient"}>
                     <div>Formularz klienta</div>
                 </Link>
-                <Link to={"/rent"}>
-                    <div>Wypożycz</div>
-                </Link>
-                <Link to={"/list"}>
-                    <div>Narty Listuj</div>
-                </Link>
-                <Link to={"/butyList"}>
-                    <div>Buty Listuj</div>
-                </Link>
                 <Link to={"/zestaw-form"}>
                     <div>Formularz zestawu</div>
                 </Link>
+                <Link to={"/rent"}>
+                    <div>Wypożycz</div>
+                </Link>
+                <Dropdown as={ButtonGroup}>
+                    <Button variant="success">Magazyn</Button>
+
+                    <Dropdown.Toggle split variant="outline-success" id="dropdown-split-basic" />
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="/list">Narty listuj</Dropdown.Item>
+                        <Dropdown.Item href="/butyList">Buty listuj</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </Container>
         </div>
     );
